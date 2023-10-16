91483cee99d12bcea9a68175783cd1d58f97021f
JelleFirlefyn <48584903+JelleFirlefyn@users.noreply.github.com>
2023-10-16 11:12:15 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+// server.js
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+
+// Dummy Data
+const items = [
+  { id: 1, name: "Item 1" },
+  { id: 2, name: "Item 2" },
+];
+
+app.get("/items", (req, res) => {
+  res.json(items);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
